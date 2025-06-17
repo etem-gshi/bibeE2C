1c2f98fced9fb38250d2a1f792462f2a4c8b00ca
etem-gshi <etem.gashi09@gmail.com>
2025-06-17 11:47:14 +0200

    intégration jeu dans html game over

--- script/test.js
+++ script/test.js
@@ -1,5 +1,5 @@
 
-let targuetNumber = Math.floor(Math.random()*100)+1;
+/*--let targuetNumber = Math.floor(Math.random()*100)+1;
 console.log(targuetNumber);
 
 let userNumber = prompt('Devinez un nombre entre 5 et 50 :');
@@ -15,9 +15,48 @@ while(userNumber != targuetNumber) {
     userNumber = prompt('Devinez un nombre entre 5 et 50 :')
 
 }
-alert(`bravos, vous avez trouvez`);
+alert(`bravos, vous avez trouvez`);*/
+
+let trigger = document.querySelector('#send-button')
+//let exemple = document.queryselectorAll('div');
+let numnerZone = document.querySelector('#user-number');
+let displayZone = document.querySelector('#game-display');
+
+let targuetNumber = Math.floor(Math.random()*100)+1;
+
+console.log(trigger,numnerZone,displayZone,targuetNumber);
+
+function compareNumber() {
+    //console.log("ça marche")
+    let userNumber = numnerZone.value;
+    let result;
+    //console.log(userNumber);
+    if(userNumber==targuetNumber){
+        result = 'gagné'
+    }else if(userNumber > targuetNumber){
+      result = `c'est trop grand!`;
+    } else{
+        result = `C'est trop petit`;
+    }
+
+    let resultZone = document.createElement('p');
+    resultZone.innerHTML = result;
+    resultZone.style.padding = "15px";
+    resultZone.classList.add('result-line');
 
 
+     //console.log(resultZone)
+    displayZone.appendChild(resultZone);
+}
+
+    
+    
+
+   
+
+
+
+trigger.addEventListener('click',compareNumber);
 
 
 
