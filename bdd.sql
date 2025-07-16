d4268dbf8c7757a95930ad47b00779bb1a662a7c
etem-gshi <etem.gashi09@gmail.com>
2025-07-16 11:54:31 +0200

    Resau2

--- bdd.sql
+++ bdd.sql
@@ -39,3 +39,84 @@ SELECT * FROM user;
 SELECT pseudo, mail, avatar FROM user;
 SELECT pseudo, mail FROM user WHERE pseudo = "Haribo";
 SELECT pseudo, mail, avatar FROM user ORDER BY mail DESC;
+
+/*supprimer des données */
+DELETE FROM user WHERE pseudo = "motdepasse";
+
+/* modification de table*/
+ALTER TABLE user
+DROP COLUMN avatar; /* DROP -> supprimer une colonne mais peut être autre chose */
+
+ALTER TABLE user
+ADD COLUMN is_admin BOOLEAN NOT NULL DEFAULT 0; /* ADD -> ajouter une colonne ou autre , il faut définir
+les éléments de la colonne , comme quand on créé une table */
+
+ALTER TABLE user MODIFY id INT INSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY;/*Change les propriété d'une colonne*/
+
+ALTER TABLE user CHANGE pseudo user_name VARCHAR(120) NOT NULL UNIQUE;/* Change le nom est propriété d'une colonne colonne*/
+
+/*Modifier le donées*/
+UPDATE user 
+SET is_admin = 1
+WHERE id = 2
+
+CREATE TABLE IF NOT EXISTS site (
+   id TINYINT UNSIGNED NOT NULL AUTP_INCREMENT PRIMARY KEY,
+   City VARCHAR(50) NOT NULL UNIQUE 
+)ENGINE=InnoDB;
+
+INSERT INTO site (city)
+VALUES("Roubaix"),("Lille"),("Armentière"),("Saint-omer");
+
+/*créer une relation*/
+
+ALTER TABLE user
+ADD COLUMN site_id TINYINT UNSIGNED NOT NULL DEFAULT 1;
+
+ALTER TABLE user 
+ADD constraint fk_user_site
+ FOREIGN KEY (site_id)
+ REFERENCES site(id);
+
+ UPDATE user 
+ SET site_id = 2 
+ WHERE id = 3;
+
+ /*jointure*/
+CREATE VIEW user_vw as (
+ SELECT user.pseudo AS utilisateur, user.mail, site.city AS ville 
+ FROM user 
+ INNER JOIN  site 
+ ON user.site_id = site.id
+);
+
+/* on s'attaque aux livres*/
+
+CREATE TABLE IF NOT EXISTS genre(
+   id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(50) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+INSERT INTO genre (name)
+VALUES ("Roman"),("Fantasy"),("Manga"),("Science Fiction"),("Horreur"),("Romance"),("Policier"),("Magazine"),("BD"),("Histoir"),("Biographie"),("Politique"),("Scolaire");
+
+CREATE TABLE IF NOT EXISTS book (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   titre VARCHAR(225) NOT NULL DEFAULT "Inconnu",
+   auteur VARCHAR(100) NOT NULL DEFAULT "Inconnu",
+   synopsis TEXT,
+   page SMALLINT NOT NULL,
+   year SMALLINT NOT NULL DEFAULT "0",
+   genre_id TINYINT UNSIGNED NOT NULL DEFAULT 1,
+   site_id TINYINT UNSIGNED NOT NULL DEFAULT 1,
+   user_id SMALLINT UNSIGNED,
+   CONSTRAINT fk_book_genre
+   FOREIGN KEY (genre_id)
+   REFERENCES genre (id),
+   CONSTRAINT fk_book_site
+   FOREIGN KEY (site_id)
+   REFERENCES site (id),
+   CONSTRAINT fk_book_user
+   FOREIGN KEY (user_id)
+   REFERENCES user(id)
+)ENGINE=InnoDB;
